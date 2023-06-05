1d3995eabdfe69771082020d47e4737e5f24a77a
gflychee <gflychee@sina.com>
2023-06-05 18:46:47 +0800

    1

--- query-ctp-option-spot.sh
+++ query-ctp-option-spot.sh
@@ -15,9 +15,7 @@ fi
 ulimit -c unlimited
 cd ~/ctp_query_stock_option
 export PATH=$PATH:~/ctp_query_stock_option/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.local/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/ctp_query_stock_option/lib
 killall ctp-query-stock-option
 ctp-query-stock-option TraderAccount_option.cfg ${DT}-option &> query-ctp-option.log
 cp -r ${DT}-option /mnt/nas-3/OPTION/ctp_option/${DT}
-ctp-query-stock-option TraderAccount_spot.cfg ${DT}-spot &> query-ctp-spot.log
-cp -r ${DT}-spot /mnt/nas-3/OPTION/ctp_spot/${DT}
